7fe48f32de92bada86c0f6d92022a9d94cfb4aef
Ely Lucas <ely@meta-tek.net>
2023-01-19 10:29:32 -0700

    chore: oops wrong config

--- client-react/cypress.config.ts
+++ client-react/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 import { createHero, deleteHero } from './cypress/support/data';
 
 export default defineConfig({
+  projectId: 'nd8nd1',
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
